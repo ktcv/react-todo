118afe90b480ff2a465ba5247751257cad3472d8
Kevin Chen <ktchenvideo@gmail.com>
2020-10-16 16:45:36 +1100

    Updates

---
+++ precache-manifest.19e091e32d97e8127dce8f4e30a0c73b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7462cfdb891861f8c88579ec0f3a727e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9016d8fd585d00f9b1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e848a3b1612add633c7",
+    "url": "/static/js/2.be9acd18.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.be9acd18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9016d8fd585d00f9b1",
+    "url": "/static/js/main.9ed127c0.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
