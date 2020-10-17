a16bd39b77b6c11d7670b1655ca47f2074714d80
Kevin Chen <ktchenvideo@gmail.com>
2020-10-17 11:39:27 +1100

    Removed delete project modal

--- src/components/IndividualProject.jsx
+++ src/components/IndividualProject.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FaTrashAlt } from 'react-icons/fa'
 import { useProjectsValue, useSelectedProjectValue } from '../context'
 import { firebase } from '../firebase'
